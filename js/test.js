0d0873bc11b5b2823a475896e4e9162845d8e144
MT <378395939@qq.com>
2016-02-28 23:35:15 +0800

    test3

--- js/test.js
+++ js/test.js
@@ -1,6 +1,8 @@
 /*
 *date:2016-2-22
 */
+var myScroll;
 $(document).ready(function(){
-	
+	myScroll = new IScroll('#test', { mouseWheel: true });
 });
+document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
