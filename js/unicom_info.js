8fbdf6c1c58b1859af19c07f22062a4f0118612e
CHP <378395939@qq.com>
2016-03-07 10:40:00 +0800

    添加产品JSON

--- js/unicom_info.js
+++ js/unicom_info.js
@@ -2,72 +2,23 @@
 *date:2016-2-22
 */
 //生成产品页面方法
+var production;
 function Product_Html(product_num,product_name){
-    var production=[
-        {
-            "name":"销售助理",
-            "logo_url":"",
-            "tag":["移动考勤","经营分析"],
-            "describtion":"功能说明简介<br/>功能1<br/>功能2",
-            "QR_url":["XSZL_QR_IOS.png","XSZL_QR_Android.png"],
-            "fuction_png_url":3
-        },
-        {
-            "name":"会议通",
-            "logo_url":"img/HYT_LOGO.png",
-            "tag":["组织会议","移动签到","电子资料"],
-            "describtion":"<p>功能说明简介<br/>功能1<br/>功能2</p>",
-            "QR_url":["HYT_QR_IOS.png","HYT_QR_Android.png"],
-            "fuction_png_url":3
-        },
-        {
-            "name":"幸福工厂",
-            "logo_url":"",
-            "tag":["员工娱乐","员工交流"],
-            "describtion":"功能说明简介<br/>功能1<br/>功能2",
-            "QR_url":["HYT_QR_IOS.png",""],
-            "fuction_png_url":3
-        },
-        {
-            "name":"工作助理",
-            "logo_url":"",
-            "tag":["手机考勤","移动办公","信息发布"],
-            "describtion":"功能说明简介<br/>功能1<br/>功能2",
-            "QR_url":["HYT_QR_IOS.png",""],
-            "fuction_png_url":3
-        },
-        {
-            "name":"卫监助手",
-            "logo_url":"",
-            "tag":["移动巡检","上报统计","信息反馈"],
-            "describtion":"功能说明简介<br/>功能1<br/>功能2",
-            "QR_url":["HYT_QR_IOS.png",""],
-            "fuction_png_url":3
-        },
-        {
-            "name":"企业远教",
-            "logo_url":"",
-            "tag":["人员管理","远程教育"],
-            "describtion":"功能说明简介<br/>功能1<br/>功能2",
-            "QR_url":["HYT_QR_IOS.png",""],
-            "fuction_png_url":3
-        },
-        {
-            "name":"智慧医院",
-            "logo_url":"",
-            "tag":["移动办公","院长查询","移动监控","智慧查房"],
-            "describtion":"功能说明简介<br/>功能1<br/>功能2",
-            "QR_url":["HYT_QR_IOS.png",""],
-            "fuction_png_url":3
+    $.ajax({
+        url:"js/production.json",
+        async:false,
+        dateType:"json",
+        success:function(result){
+            production=result;
         }
-    ];
+    })
     $("#info_title").html("");
     $("#info_title").append('<h1 class="no-top-margin">'+production[product_num].name+'</h1>');
     for(var i=0;i<production[product_num].tag.length;i++) {
         $('<span>').addClass("badge").text(production[product_num].tag[i]).appendTo("#info_title");
     }
     $("#info_logo").children("img").attr("src","img/logo/"+product_name+"_LOGO.png");
-    $("#info_description").html(production[product_num].describtion);
+    $("#info_description").children("div").children("p").html(production[product_num].describtion);
     $("#info_QR").html("");
     for(var i=0;i<production[product_num].QR_url.length;i++) {
         if (production[product_num].QR_url[i]!="")
