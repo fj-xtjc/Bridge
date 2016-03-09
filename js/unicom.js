1fcb54210108d32db5a9cd653db33d6d8c5eb4b8
MT <378395939@qq.com>
2016-03-09 23:07:00 +0800

    23:06

--- js/unicom.js
+++ js/unicom.js
@@ -1,44 +1,44 @@
 /*
 *date:2016-2-22
 */
-function product_btn_html(product_name){
+function products_btn_html(result,i){
+	$("<div>").addClass("col-md-4 col-sm-6 col-xs-12").html(
+	'<button type="button" class="btn btn-default  btn-block btn-md btn_div" name="'+result[0]["app"][i]+'"data-appInfo="'+result[1][result[0]["app"][i]].app_name+'"> ' +
+		'<div class="col-md-4 col-sm-4 col-xs-4"> ' +
+			'<img src="img/logo/'+result[1][result[0]["app"][i]].logo_url+'" class="app_Logo"> ' + 
+		'</div> ' +
+		'<div class="col-md-6 col-sm-6 col-xs-6"> ' +
+			'<h3 class="strong">'+result[1][result[0]["app"][i]].app_name +'</h3> ' +
+			'<p class="small">产品即将上线，敬请期待</p> ' +
+		'</div> ' +
+		'<div class="col-md-2 col-sm-2 col-xs-2"> ' +
+			'<div class="row"> ' +
+				'<span class="badge col-md-12 col-sm-12 col-xs-12">安卓</span> ' +
+				'<span class="badge col-md-12 col-sm-12 col-xs-12">IOS</span> ' +
+			'</div> ' +
+		'</div> ' +
+	'</button>').appendTo("#index_unicom");
+}
+$(document).ready(function(){
+	var bg_width = parseInt($(window).width()) - 80;
+	var bg_height = parseInt(bg_width*2/3 - 5) + "px";
+	$(".bg_unicom_app").css("height","500px");
+	$("#index_unicom").html("");
 	$.ajax({
 		type:"get",
 		url:"js/production.json",
 		async:false,
 		dateType:"json",
-		success:function(production){
-			$("<div>").addClass("col-md-4 col-sm-6 col-xs-12").html(
-			'<button type="button" class="btn btn-default  btn-block btn-md btn_div" name="'+product_name+'"data-appInfo="'+production[product_name].name+'"> ' +
-				'<div class="col-md-4 col-sm-4 col-xs-4"> ' +
-					'<img src="img/images_12.png" class="btn_img"> ' +
-				'</div> ' +
-				'<div class="col-md-6 col-sm-6 col-xs-6"> ' +
-					'<h3 class="strong">'+production[product_name].name +'</h3> ' +
-					'<p class="small">产品即将上线，敬请期待</p> ' +
-				'</div> ' +
-				'<div class="col-md-2 col-sm-2 col-xs-2"> ' +
-					'<div class="row"> ' +
-						'<span class="badge col-md-12 col-sm-12 col-xs-12">安卓</span> ' +
-						'<span class="badge col-md-12 col-sm-12 col-xs-12">IOS</span> ' +
-					'</div> ' +
-				'</div> ' +
-			'</button>').appendTo("#index_unicom");
+		success:function(result){
+			for (var i = 0; i < result[0]["app"].length; i++) {
+				products_btn_html(result,i);
+			};
+		},
+		error:function(){
+			alert("加载失败!");
 		}
 	});
-}
-$(document).ready(function(){
-	var bg_width = parseInt($(window).width()) - 80;
-	var bg_height = parseInt(bg_width*2/3 - 5) + "px";
-	$(".bg_unicom_app").css("height",$(window).height()-80);
-	// $("#index_unicom").html("");
-	// product_btn_html("XSZL");
-	// product_btn_html("HYT");
-	// product_btn_html("XFGC");
-	// product_btn_html("GZZL");
-	// product_btn_html("WJZS");
-	// product_btn_html("QYYJ");
-	// product_btn_html("ZHYY");
+
 	$("button[data-appInfo]").click(function() {
 		location.href = "unicom_app_info.html?page=" + $(this).attr('name');
 	});
--- js/unicom_info.js
+++ js/unicom_info.js
@@ -10,20 +10,20 @@ function Product_Html(product_name){
         dateType:"json",
         success:function(production){
             $("#info_title").html("");
-            $("#info_title").append('<h1 class="no-top-margin">'+production[product_name].name+'</h1>');
-            for(var i= 0;i<production[product_name].tag.length;i++) {
-                $('<span>').addClass("badge").addClass("bg_badge"+i%5).text(production[product_name].tag[i]).appendTo("#info_title");
+            $("#info_title").append('<h1 class="no-top-margin">'+production[1][product_name].app_name+'</h1>');
+            for(var i= 0;i<production[1][product_name].tag.length;i++) {
+                $('<span>').addClass("badge").addClass("bg_badge"+i%5).text(production[1][product_name].tag[i]).appendTo("#info_title");
             }
-            $("#info_logo").children("img").attr("src","img/logo/"+production[product_name].logo_url);
-            $("#info_description").children("div").children("p").html(production[product_name].describtion);
+            $("#info_logo").children("img").attr("src","img/logo/"+production[1][product_name].logo_url);
+            $("#info_description").children("div").children("p").html(production[1][product_name].describtion);
             $("#info_QR").html("");
-            for(var i=0;i<production[product_name].QR_url.length;i++) {
-                if (production[product_name].QR_url[i]!="")
-                    $('<img>').addClass("img-rounded QR_Code").attr("src","img/QR_Code/" + production[product_name].QR_url[i]).appendTo("#info_QR");
+            for(var i=0;i<production[1][product_name].QR_url.length;i++) {
+                if (production[1][product_name].QR_url[i]!="")
+                    $('<img>').addClass("img-rounded QR_Code").attr("src","img/QR_Code/" + production[1][product_name].QR_url[i]).appendTo("#info_QR");
             }
             $("#carousel_ol").html("");
             $("#carousel_img").html("");
-            for(var i= 0,j=1;i<production[product_name].fuction_png_url;i++,j++){
+            for(var i= 0,j=1;i<production[1][product_name].fuction_png_url;i++,j++){
                 if(i==0) {
                     $('<li>').addClass("active").attr("data-target","#app_info_Carousel").attr("data-slide-to",i).appendTo("#carousel_ol");
                     $('<div>').addClass("item active").appendTo("#carousel_img").append('<img src="img/product_pic/'+product_name+'_Pic'+ j +'.png">');
